c0607aa5ce49c554f10a3c84aad6dc5e43844929
luongtrantronghuy <39868850+luongtrantronghuy@users.noreply.github.com>
2021-12-24 11:06:35 -0800

    finished skills

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -31,3 +31,23 @@ function linkAction() {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// Skills
+const skillsContent = document.getElementsByClassName('skills__content')
+const skillsHeader = document.querySelectorAll('.skills__header')
+
+function skillsToggle(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass == 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', skillsToggle)
+})
